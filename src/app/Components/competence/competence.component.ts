22a4239f22b5b838728472c7f7edf64a9a558f0a
abdessalem <slouma4ghodbeny@gmail.com>
2025-01-09 22:28:49 +0100

    compétence

---
+++ src/app/Components/competence/competence.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competence',
+  standalone: true,
+  imports: [],
+  templateUrl: './competence.component.html',
+  styleUrl: './competence.component.scss',
+})
+export class CompetenceComponent {}
--- src/app/Home/home/home.component.ts
+++ src/app/Home/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FirsPresentationComponent } from '../../Components/firs-presentation/firs-presentation.component';
 import { ParcoursAccademiqueComponent } from '../../Components/parcours-accademique/parcours-accademique.component';
 import { ExpProfessionnelComponent } from '../../Components/exp-professionnel/exp-professionnel.component';
+import { RecapExpProfessionnelComponent } from '../../Components/recap-exp-professionnel/recap-exp-professionnel.component';
+import { CompetenceComponent } from '../../Components/competence/competence.component';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +11,8 @@ import { ExpProfessionnelComponent } from '../../Components/exp-professionnel/ex
     FirsPresentationComponent,
     ParcoursAccademiqueComponent,
     ExpProfessionnelComponent,
+    RecapExpProfessionnelComponent,
+    CompetenceComponent,
   ],
   standalone: true,
   templateUrl: './home.component.html',
